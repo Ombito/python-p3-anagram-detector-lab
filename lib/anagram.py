7905242720d16c72d83645d35df99744ce7fc042
ombito <alvinombito09@gmail.com>
2023-09-18 12:55:23 +0300

    Complete assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    def match(self, word_list): 
+        anagrams = [letter for letter in word_list if self.is_anagram(letter)]
+        return anagrams 
+    def is_anagram(self, new_word):
+        return sorted(self.word.lower()) == sorted(new_word.lower())
